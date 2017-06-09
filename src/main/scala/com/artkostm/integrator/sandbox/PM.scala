9a960fdc7cf246cc850666bac4e9f29bf524cae6
Artsiom <artkostm@gmail.com>
2017-06-09 22:39:20 +0300

    add pattern matching examples

---
+++ src/main/scala/com/artkostm/integrator/sandbox/PM.scala
@@ -0,0 +1,19 @@
+/**
+  * Created by artsiom.chuiko on 09/06/2017.
+  */
+object Pm extends App {
+  val arr = Array("1", "2", "3", "4")
+  val tuple: (String, String, String, String) = ("1", "2", "3", "4")
+
+  arr match {
+    case Array("1", _*) => println("1!")
+  }
+
+  arr match {
+    case Array("1", rest@_*) => println(s"rest: $rest")
+  }
+
+  tuple.productIterator.toList match {
+    case _ :: "2" :: rest => println(s"Wow! rest: $rest")
+  }
+}
