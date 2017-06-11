bfd94274cf92688a3045c82141392ffc9e13a8ef
Artsiom <artkostm@gmail.com>
2017-06-11 22:03:14 +0300

    added macroGL example

---
+++ src/main/scala/com/artkostm/integrator/sandbox/MacroGL.scala
@@ -0,0 +1,19 @@
+/**
+  * Created by arttsiom.chuiko on 10/06/2017.
+  */
+//import org.lwjgl.opengl._
+//import org.macrogl._
+//
+//object MacroGL extends App {
+//  val contextAttributes = new ContextAttribs(3, 2)
+//    .withForwardCompatible(true).withProfileCore(true)
+//
+//  Display.setDisplayMode(new DisplayMode(800, 600))
+//  Display.create(new PixelFormat, contextAttributes)
+//
+//  while (!Display.isCloseRequested) {
+//    Display.update()
+//  }
+//
+//  Display.destroy()
+//}
