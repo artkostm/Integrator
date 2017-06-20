8e112d91b5c779f8027085e61bd81800a1350546
Artsiom <artkostm@gmail.com>
2017-06-21 00:04:32 +0300

    deep learning library

---
+++ src/main/scala/com/artkostm/integrator/sandbox/Nd4j.scala
@@ -0,0 +1,10 @@
+/**
+  * Created by artsiom.chuiko on 21/06/2017.
+  */
+import org.nd4s.Implicits._
+//import org.nd4j.linalg.factory.Nd4j
+
+object Application extends App {
+  val arr = (1 to 9).asNDArray(3,3)
+  println(arr)
+}
