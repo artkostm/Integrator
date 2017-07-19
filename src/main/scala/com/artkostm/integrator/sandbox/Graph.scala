0f49c77f220cda68917200487b1831e750eb854f
Artsiom <artkostm@gmail.com>
2017-07-19 23:10:09 +0300

    more complex example

--- src/main/scala/com/artkostm/integrator/sandbox/Graph.scala
+++ src/main/scala/com/artkostm/integrator/sandbox/Graph.scala
@@ -1,6 +1,6 @@
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Source
+import akka.stream._
+import akka.stream.scaladsl.{Balance, Broadcast, Flow, GraphDSL, Merge, RunnableGraph, Sink, Source}
 
 /**
   * Created by artsiom.chuiko on 18/07/2017.
@@ -11,7 +11,33 @@ object Main extends App {
 
   implicit val dispatcher = system.dispatcher
 
-  val future = Source(1 to 10).runForeach(println)
+  /**
+    * http://doc.akka.io/docs/akka/current/images/compose_graph.png
+    *            ________
+    *            |      |
+    *            | C -> |
+    *            |      |
+    *   A -> B ->|-------
+    *            |      |     F
+    *            | D -> | E ->
+    *            |______|         G
+    *
+    *
+    */
+  import akka.stream.scaladsl.GraphDSL.Implicits._
+  val graph = GraphDSL.create() { implicit builder =>
+    val B: FlowShape[Int, Int]          = builder.add(Flow[Int].map(_ * 2))
+    val D: FlowShape[Int, Int]          = builder.add(Flow[Int].map(_ + 1))
+    val E: UniformFanOutShape[Int, Int] = builder.add(Balance[Int](2))
+    val G: UniformFanInShape[Int, Int]  = builder.add(Merge[Int](2))
+
+
+            B  ~>  D  ~>  E  ~>  G
+                          E  ~>  G
+    FlowShape(B.in, G.out)
+  }.named("Koko")
+
+  val future = Source(1 to 10).via(graph).runForeach(println)
 
   future.onComplete(_ => system.terminate())
 }
