93ecad674542c670be7e490b7e619f37d91a803e
Artsiom <artkostm@gmail.com>
2017-09-17 16:04:47 +0300

    request sample

--- core/src/main/scala/com/artkostm/integrator/routing/Request.scala
+++ core/src/main/scala/com/artkostm/integrator/routing/Request.scala
@@ -1,5 +1,11 @@
 package com.artkostm.integrator.routing
 
-class Request {
+import scala.language.dynamics
 
+case class Query [integrator](params: Map[String, String]) extends Dynamic {
+  def selectDynamic(tag: String): Option[String] = params.get(tag)
+}
+
+class Request(params: Map[String, String]) {
+  lazy val query = Query(params)
 }
