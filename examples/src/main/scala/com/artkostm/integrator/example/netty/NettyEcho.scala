5f2ba14969ad8bb2b5f15c26fc8ad6e4b1c4d706
Artsiom <artkostm@gmail.com>
2017-10-01 23:02:17 +0300

    update echo server example

--- examples/src/main/scala/com/artkostm/integrator/example/netty/NettyEcho.scala
+++ examples/src/main/scala/com/artkostm/integrator/example/netty/NettyEcho.scala
@@ -1,20 +1,52 @@
 import java.net.InetSocketAddress
 
+import akka.actor.ActorSystem
 import io.netty.bootstrap.ServerBootstrap
-import io.netty.buffer.{ByteBuf, DefaultByteBufHolder, PooledByteBufAllocator, Unpooled}
+import io.netty.buffer._
 import io.netty.channel.ChannelHandler.Sharable
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.nio.NioServerSocketChannel
 import io.netty.channel._
+import io.netty.channel.group.DefaultChannelGroup
 import io.netty.channel.socket.SocketChannel
+import io.netty.handler.codec.http.HttpServerUpgradeHandler
 import io.netty.util.CharsetUtil
 
+import scala.concurrent.duration.Duration
+import scala.concurrent._
+
 @Sharable
-class EchoServerHandler extends ChannelInboundHandlerAdapter {
+class EchoServerHandler(implicit val dispatcher: ExecutionContextExecutor) extends ChannelInboundHandlerAdapter {
   override def channelRead(ctx: ChannelHandlerContext, msg: scala.Any): Unit = {
+
     val in = msg.asInstanceOf[ByteBuf]
     println(s"server received: ${in.toString(CharsetUtil.UTF_8)}")
-    ctx.write(in, ctx.voidPromise())
+    val promise = Promise[Unit]()
+    promise.future onComplete {
+      res => println("From future1: " + res)
+    }
+
+    promise.future onComplete {
+      res => println("From future2: " + res)
+    }
+
+    promise.future onComplete {
+      res => println("From future3: " + res)
+    }
+    ctx.write(in).addListener((future: ChannelFuture) => {
+      if (future.isSuccess) promise.success()
+      else promise.failure(future.cause())
+    })
+
+
+    //ctx.fireChannelRead()
+//    Future {
+//      val in = msg.asInstanceOf[ByteBuf]
+//      ctx.write(in, ctx.voidPromise())
+//      s"server received: ${in.toString(CharsetUtil.UTF_8)}"
+//    } onComplete {
+//      res => println(res)
+//    }
   }
 
   override def channelReadComplete(ctx: ChannelHandlerContext): Unit = {
@@ -30,17 +62,23 @@ class EchoServerHandler extends ChannelInboundHandlerAdapter {
 }
 
 object Server extends App {
+  val system = ActorSystem()
+  implicit val dispatcher = system.dispatcher
+  val group = new NioEventLoopGroup(4, system.dispatcher)
+  val allChannels = new DefaultChannelGroup(group.next())
+
+
   def start(): Unit = {
     //DefaultByteBufHolder
-    Epoll
+    //Epoll
     val echoHandler = new EchoServerHandler()
-    val group = new NioEventLoopGroup()
+
     try {
       val bootstrap = new ServerBootstrap()
       bootstrap.group(group)
         .channel(classOf[NioServerSocketChannel])
         .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT) //java -Dio.netty.allocator.numDirectArenas=32 -Dio.netty.allocator.numHeapArenas=32
-        //.childOption(ChannelOption.WRITE_BUFFER_WATER_MARK, 8 * 1024)
+        .option(ChannelOption.WRITE_BUFFER_WATER_MARK, new WriteBufferWaterMark(8 * 1024, 32 * 1024))
         .localAddress(new InetSocketAddress(8080))
         .childHandler(new ChannelInitializer[SocketChannel] {
           override def initChannel(ch: SocketChannel) = ch.pipeline().addLast(echoHandler)
@@ -52,5 +90,12 @@ object Server extends App {
     }
   }
 
+  val buf = Unpooled.copiedBuffer("Hello bitch!".getBytes)
+  println(ByteBufUtil.prettyHexDump(buf))
+  println(new String(ByteBufUtil.decodeHexDump("48656c6c6f20626974636821")))
+  //  io.netty.handler.codec.http.HttpResponseStatus.SWITCHING_PROTOCOLS
+  //  io.netty.handler.codec.http.HttpServerExpectContinueHandler
+  println(Await.result(Future { "Hello!" }, Duration.Inf))
+//  HttpServerUpgradeHandler
   start()
 }
