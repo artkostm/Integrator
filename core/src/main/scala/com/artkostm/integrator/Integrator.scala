0f8c3642b4cdf855cb91058e0e979687ca517bb3
Artsiom <artkostm@gmail.com>
2017-10-07 20:44:23 +0300

    add entry point of app

--- core/src/main/scala/com/artkostm/integrator/Integrator.scala
+++ core/src/main/scala/com/artkostm/integrator/Integrator.scala
@@ -1,5 +1,18 @@
 package com.artkostm.integrator
 
-class Integrator {
+import akka.actor.ActorSystem
 
+import scala.util.control.NonFatal
+
+object Integrator {
+  lazy val ACTOR_SYSTEM_NAME = "Integrator"
+  lazy val system = ActorSystem(ACTOR_SYSTEM_NAME)
+
+  lazy val config = try { system.settings.config } catch {
+    case NonFatal(e) =>
+      println(s"Could not load application.conf. Details: ${e.getMessage}")
+      throw e
+  }
 }
+
+
