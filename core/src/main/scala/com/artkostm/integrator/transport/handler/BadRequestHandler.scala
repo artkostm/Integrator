b2aa76bf9d1f5c5d8502c90aaec17bae2a850c14
Artsiom <artkostm@gmail.com>
2017-10-08 16:28:05 +0300

    initial implementation of bad request handler

--- core/src/main/scala/com/artkostm/integrator/transport/handler/BadRequestHandler.scala
+++ core/src/main/scala/com/artkostm/integrator/transport/handler/BadRequestHandler.scala
@@ -1,5 +1,24 @@
 package com.artkostm.integrator.transport.handler
 
-object BadRequestHandler {
+import io.netty.channel.ChannelHandler.Sharable
+import io.netty.channel.{ChannelHandlerContext, SimpleChannelInboundHandler}
+import io.netty.handler.codec.http.HttpMessage
+import io.netty.util.ReferenceCountUtil
 
+@Sharable
+private[transport] object BadRequestHandler extends SimpleChannelInboundHandler[HttpMessage] {
+
+  override def channelRead0(ctx: ChannelHandlerContext, msg: HttpMessage): Unit =
+    if (!msg.decoderResult().isFailure) ctx.fireChannelRead(msg)
+    else {
+      ReferenceCountUtil.release(msg)
+      handleException(ctx, msg.decoderResult().cause())
+    }
+
+  private[this] def handleException(ctx: ChannelHandlerContext, throwable: Throwable): Unit = {
+    val response = exceptionToResponse(throwable)
+
+  }
+
+  def exceptionToResponse(throwable: Throwable) = ???
 }
