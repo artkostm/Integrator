fa72859d32712f5dcd79ed52f10c989be3fa8804
Artsiom Chuiko <v-achuiko@expedia.com>
2017-10-10 19:06:25 +0300

    add callback actor

---
+++ core/src/main/scala/com/artkostm/integrator/watcher/CallbackActor.scala
@@ -0,0 +1,36 @@
+package com.artkostm.integrator.watcher
+
+import akka.actor.{ActorLogging, Actor, Props}
+import com.artkostm.integrator.watcher.Messages.PerformCallback
+
+/**
+  * Companion object for CallbackActor to allow easy creation of
+  * props via apply method
+  */
+object CallbackActor {
+
+  /**
+    * Factory method for props required to spawn a MonitorActor
+    * @return Props for spawning an actor
+    */
+  def apply() = Props(classOf[CallbackActor])
+}
+
+/**
+  * Actor that performs Callbacks
+  *
+  * This allows us to rather easily control concurrency in the
+  * parent MonitorActor by simply spawning a certain number of
+  * actors in the routing pool.
+  *
+  * Should be created via companion method's props
+  */
+class CallbackActor extends Actor with ActorLogging {
+  def receive = {
+    case PerformCallback(path, callback) => {
+      log.debug(s"Performing callback for path: $path")
+      callback(path)
+    }
+    case _ => log.error("Unknown message received")
+  }
+}
