3960a9f71c643d61c732b72ecd742efbffab6908
Artsiom Chuiko <v-achuiko@expedia.com>
2017-10-10 16:25:05 +0300

    small update

--- core/src/main/scala/com/artkostm/integrator/routing/Request.scala
+++ core/src/main/scala/com/artkostm/integrator/routing/Request.scala
@@ -4,6 +4,8 @@ import scala.language.dynamics
 
 case class Query [integrator](params: Map[String, String]) extends Dynamic {
   def selectDynamic(tag: String): Option[String] = params.get(tag)
+  
+  def some(): Unit = {}
 }
 
 class Request(params: Map[String, String]) {
