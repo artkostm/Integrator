c3b95756326e0ca1951aa9075245fb789e96f95e
Artsiom <artkostm@gmail.com>
2017-10-19 23:14:24 +0300

    add example of channel initialization with server config

---
+++ examples/src/main/scala/com/artkostm/integrator/example/netty/Pipeline.scala
@@ -0,0 +1,48 @@
+package com.artkostm.integrator.example.netty
+
+import java.net.InetSocketAddress
+
+import cats.instances.option._
+import cats.data.Kleisli
+import io.netty.bootstrap.ServerBootstrap
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.{ChannelInboundHandlerAdapter, ChannelInitializer}
+import io.netty.channel.socket.SocketChannel
+import io.netty.channel.socket.nio.{NioServerSocketChannel, NioSocketChannel}
+
+object Pipeline extends App {
+
+  val initializer = new ChannelInitializer[SocketChannel] {
+    override def initChannel(ch: SocketChannel): Unit = {
+      val pipeline = ch.pipeline()
+      val injector = for {
+        handler1 <- Handler1().local[ServerConfig](_.netty)
+      } yield pipeline.addLast("first", handler1)
+
+      injector.run(ServerConfig(NettyConfig("1.0")))
+      import scala.collection.JavaConverters._
+      println(pipeline.toMap.asScala.map(entr => entr._2))
+    }
+  }
+
+  val group = new NioEventLoopGroup()
+  val bootstrap = new ServerBootstrap()
+  bootstrap.group(group)
+    .channel(classOf[NioServerSocketChannel])
+    .localAddress(new InetSocketAddress(8189))
+    .childHandler(initializer)
+  val chFuture = bootstrap.bind().sync()
+  chFuture.channel().closeFuture().sync()
+  group.shutdownGracefully().sync()
+
+}
+
+case class ServerConfig(netty: NettyConfig)
+case class NettyConfig(version: String)
+class Handler1 extends ChannelInboundHandlerAdapter {
+
+}
+
+object Handler1 {
+  def apply(): Kleisli[Option, NettyConfig, Handler1] = Kleisli(_ => Option(new Handler1))
+}
